3dd5edef17e84be90f537b72faef999511498f01
Tatiana Smolin <tatismolin@gmail.com>
2020-01-26 23:44:32 -0700

    Switches fetch to heroku.

--- src/components/Store.js
+++ src/components/Store.js
@@ -16,8 +16,8 @@ class Store extends Component{
 
     componentDidMount(){
         // fetch(`${aws}/items`)
-        fetch(`${localhostURL}/items`)
-        // fetch(`${herokuURL}/items`)
+        // fetch(`${localhostURL}/items`)
+        fetch(`${herokuURL}/items`)
             .then(response => response.json())
             .then(response => response)
             .then(items => {
